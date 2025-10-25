320eedf7c3f4b04bc8bd15cebce4149b88dfbb9d
MohammadAlamin99 <alamin.cse.99@gmail.com>
2025-10-26 02:03:00 +0600

    updated atc button on shop page

--- src/components/Shop/ShopProductCard.tsx
+++ src/components/Shop/ShopProductCard.tsx
@@ -104,13 +104,15 @@ const ShopProductCard = ({
                     alt="best seller"
                   />
                 </Link>
-                <button
-                  className="font-inter text-[#FEFEFE] text-[16px] font-medium bg-[#141718]
+                <Link href={`/product/${item?._id}`}>
+                  <button
+                    className="font-inter text-[#FEFEFE] text-[16px] font-medium bg-[#141718]
                 cursor-pointer leading-[28px] py-2.5 w-[80%] rounded-[8px]
                 absolute bottom-4 left-[50%] translate-x-[-50%] opacity-0 group-hover:opacity-100 transition-all duration-300"
-                >
-                  Add to cart
-                </button>
+                  >
+                    Order Now
+                  </button>
+                </Link>
               </div>
               <div className="flex items-center gap-0.5 mt-3 mb-1">
                 {Array.from({ length: 5 }).map((_, index) => (
