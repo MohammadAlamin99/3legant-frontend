a666fa9e862d566789f027e5d31feac095c672b4
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-12-16 07:47:21 +0600

    refactor: update CartDrawer to display variant options dynamically

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -99,6 +99,7 @@ export default function CartDrawer({ cartOpen, onClose }: CartDrawerProps) {
     }
   };
 
+
   return (
     <>
       <div
@@ -162,10 +163,14 @@ export default function CartDrawer({ cartOpen, onClose }: CartDrawerProps) {
                         TK. {item?.price}
                       </span>
                     </div>
-                    <p className="text-xs text-[#6C7275] font-inter mb-2">
-                      Color: {item?.options?.color} | Size:{" "}
-                      {item?.options?.size}
-                    </p>
+                    {/* variant opiton */}
+                    {item?.options && Object.keys(item.options).length > 0 && (
+                      <p className="text-xs text-[#6C7275] font-inter mb-2">
+                        {Object.entries(item.options)
+                          .map(([key, value]) => `${key}: ${value}`)
+                          .join(" | ")}
+                      </p>
+                    )}
 
                     <div className="flex justify-between items-center mt-auto">
                       {/* Quantity */}
